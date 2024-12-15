d16c9efc217d6eb92daa2808a2d2bb5800aa9330
Kevin <kevinvar2805@gmail.com>
2024-12-15 13:49:17 -0600

    Finished app.js

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
 // TODO
+const { useState } = React;
+
+const GroceryListItem = ({groceryItem}) => {
+
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovering(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovering(false);
+  };
+
+  const style = {
+    fontWeight: isHovering ? 'bold' : 'normal',
+  };
+
+  return (
+    <li style={style} onMouseEnter = {handleMouseEnter} onMouseLeave = {handleMouseLeave}>
+      {groceryItem}
+    </li>
+  );
+};
+
+const GroceryList = ({groceryItems}) => (
+  <ul>
+    {groceryItems.map((groceryItem) => <GroceryListItem groceryItem = {groceryItem} />)}
+  </ul>
+);
+
+const App = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <GroceryList groceryItems={['eggs', 'milk', 'coffee']}/>
+  </div>
+);
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
